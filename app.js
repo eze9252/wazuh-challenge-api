38fe94beabe93dd38612f4b29ad9c2d69089d2f2
eairaudo <ezequiel.airaudo@mercadolibre.com>
2020-12-06 01:54:58 -0300

    fix dependecies

--- app.js
+++ app.js
@@ -39,8 +39,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(5000, function () {
-  console.log('App listening on port 5000!')
-})
+console.log('App listening on port 5000!')
+
 
 module.exports = app;
